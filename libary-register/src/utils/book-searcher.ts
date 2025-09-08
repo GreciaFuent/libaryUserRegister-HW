d7312b41de80632c4c8d9445c7ecd7a792983df2
GreciaFuent <greciafuentes307@gmail.com>
2025-09-07 23:07:18 -0600

    feat(book): add the logic of the userSearcher in the bookSearcher

--- libary-register/src/utils/book-searcher.ts
+++ libary-register/src/utils/book-searcher.ts
@@ -1,10 +1,22 @@
 import BookRepository from "./book-repository";
+import UserSeeUsers from "./see-users";
+import Book from "./book";
 
 export default class BookSearcher {
-    private repository: BookRepository;
-    
-    constructor(repository: BookRepository) {
-        this.repository = repository
+  private repository: BookRepository;
+  private userSearcher: UserSeeUsers;
+
+  constructor(repository: BookRepository, userSearcher: UserSeeUsers) {
+    this.repository = repository;
+    this.userSearcher = userSearcher;
+  }
+
+  public run(userId: number): Book[] {
+    const user = this.userSearcher.run(userId);
+    if (!user) {
+      throw new Error(`User with id ${userId} not found`);
     }
-    // falta implementar lo del userSearcherId
+    const books = this.repository.find();
+    return books;
+  }
 }
